e7be47db6f3d0285fc50683df271ca426e7a0675
Kurumi127 <dulcey537@gmail.com>
2026-06-10 16:40:14 -0600

    Primer commit

---
+++ Back/back/src/test/java/com/Sistema/GestionTramites/GestionTramitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sistema.GestionTramites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTramitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
